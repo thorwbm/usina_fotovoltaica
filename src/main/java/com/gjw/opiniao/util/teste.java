279651c5022fcf7a7a54707c3ed4552ddcfaa62a
wemerson maduro <lagoawb@gmail.com>
2020-10-24 12:02:42 -0300

    sincronizacao

---
+++ src/main/java/com/gjw/opiniao/util/teste.java
@@ -0,0 +1,12 @@
+package com.gjw.opiniao.util;
+
+public class teste {
+
+	 public static void main(String[] args) {
+     	while (int ii = 0; ii < 2) {
+     		ii++;
+     		System.out.println("ii = " + ii);
+     	}
+     }
+     
+}
