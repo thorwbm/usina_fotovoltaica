62776598ded310a8f012c9e721f71a7b76ad6258
wemerson maduro <lagoawb@gmail.com>
2021-05-13 19:24:04 -0300

    atualizacao

--- src/main/java/com/gjw/opiniao/util/teste.java
+++ src/main/java/com/gjw/opiniao/util/teste.java
@@ -3,7 +3,9 @@ package com.gjw.opiniao.util;
 public class teste {
 
 	 public static void main(String[] args) {
-     	while (int ii = 0; ii < 2) {
+		 int ii= 0; 
+		 
+		 while (  ii < 2) {
      		ii++;
      		System.out.println("ii = " + ii);
      	}
